eee03eb96b536b48b2db6c0e2bd8357d0293d5ca
hontarukmykola <gontarykmykola111@gmail.com>
2024-04-10 15:56:01 +0300

    Add hello button and event

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace ClientApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
