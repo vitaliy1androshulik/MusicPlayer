80eb94fad26e0829dc502078bf19e52351ba3231
vitaliy1androshulik <vatalikandroshulik@gmail.com>
2024-04-10 15:19:39 +0300

    Add project files.

---
+++ ClientApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
