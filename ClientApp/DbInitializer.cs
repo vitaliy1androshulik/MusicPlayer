feb4bff79488f580b439d70bafbe2adf9737beb9
hontarukmykola <gontarykmykola111@gmail.com>
2024-04-20 17:29:52 +0300

    Created Database

---
+++ ClientApp/DbInitializer.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public static class DbInitializer
+    {
+        public static void SeedTracks(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Track>().HasData(new Track[]
+            {
+            new Track
+            {
+                Id = 1,
+                Name = "she-wolf",
+                Year = 2013,
+                Genre = "pop",
+                Language = "Ukrainian",
+                AuthorId = 1
+            },
+            new Track
+            {
+                Id = 2,
+                Name = "Despacito",
+                Year = 2017,
+                Genre = "Latin pop",
+                Language = "Spanish",
+                AuthorId = 2
+            },
+            new Track
+            {
+                Id = 3,
+                Name = "Cheri Cheri Lady",
+                Year = 1985,
+                Genre = "Pop",
+                Language = "English",
+                AuthorId = 3
+            }
+
+
+            }) ;
+        }
+        public static void SeedAuthors(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(new Author[]
+            {
+            new Author
+            {
+                Id = 1,
+                Name = "Oleg",
+                SurName = "Vinnik",
+                LastName = "Anatolyevich",
+                Nationality = "Ukraine",
+            },
+            new Author
+            {
+                Id = 2,
+                Name = "Luis",
+                SurName = "Fonsi",
+                LastName = "Rodriguez",
+                Nationality = "Puerto Rico",
+            },
+
+            new Author
+            {
+                Id = 3,
+                Name = "Dieter",
+                SurName = "Gunter",
+                LastName = "Bohlen",
+                Nationality = "Germany",
+            }
+            });
+        }
+        public static void SeedPlayLists(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayList>().HasData(new PlayList[]
+            {
+            new PlayList
+            {
+                Id= 1,
+                Name = "My",
+                Genre = "to my taste",
+                CountOfTracks = 3,
+
+            }
+
+
+
+
+            });
+        }
+    }
+}
--- ClientApp/MusicPlayerDbContext.cs
+++ ClientApp/MusicPlayerDbContext.cs
@@ -1,14 +1,108 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ClientApp
 {
+    public class Track
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public int Year { get; set; }
+        [Required, MaxLength(100)]
+        public string Genre { get; set; }
+        [Required, MaxLength(100)]
+        public string Language { get; set; }
+
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+        
+        public ICollection<PlayList> PlayLists { get; set; }
+
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public string SurName { get; set; }
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+        [Required, MaxLength(100)]
+        public string Nationality { get; set; }
+
+        public ICollection<Track> Tracks { get; set; }
+
+    }
+
+    public class PlayList
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public int CountOfTracks { get; set; }
+        [Required, MaxLength(100)]
+        public string Genre { get; set; }
+
+
+        public ICollection<Track> Tracks { get; set; }
+
+
+    }
     public class MusicPlayerDbContext : DbContext
     {
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Author> Autors { get; set; }
+        public DbSet<PlayList> PlayLists { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-L9K9OL7\SQLEXPRESS;
+                                        Initial Catalog = MusicPlayer;
+                                        Integrated Security=True;
+                                        Connect Timeout=30;
+                                        Encrypt=False;
+                                        Trust Server Certificate=False;
+                                        Application Intent=ReadWrite;
+                                        Multi Subnet Failover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //one to one
+            modelBuilder.Entity<Track>()
+                .HasOne(f => f.Author)
+                .WithMany(f => f.Tracks)
+                .HasForeignKey(f => f.AuthorId);
+
+
+
+            //many to many
+
+            modelBuilder.Entity<Track>()
+                .HasMany(f => f.PlayLists)
+                .WithMany(c => c.Tracks);
+
+
+            modelBuilder.SeedAuthors();
+            modelBuilder.SeedPlayLists();
+            modelBuilder.SeedTracks();
+
+
+
+
+
+        }
     }
 }
