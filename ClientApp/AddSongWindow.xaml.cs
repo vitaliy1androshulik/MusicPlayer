62b4c90b76cf45e56f221d733cdc333a414e2034
vitaliy1androshulik <vatalikandroshulik@gmail.com>
2024-04-29 15:32:54 +0300

    New smal design

--- ClientApp/AddSongWindow.xaml.cs
+++ ClientApp/AddSongWindow.xaml.cs
@@ -24,12 +24,20 @@ namespace ClientApp
     public partial class AddSongWindow : Window
     {
         Viewmodel model;
+        MusicPlayerDbContext context;
         public AddSongWindow()
         {
             InitializeComponent();
             model = new Viewmodel();
             this.DataContext = model;
             
+        }
+        public AddSongWindow(MusicPlayerDbContext Context)
+        {
+            InitializeComponent();
+            model = new Viewmodel();
+            this.DataContext = model;
+            this.context = Context;
         }
 
         private void BtnFilePath_Click(object sender, RoutedEventArgs e)
@@ -54,9 +62,9 @@ namespace ClientApp
                 track.Language = model.TrackLanguage;
                 track.Author = model.TrackAuthor;
                 track.Year = model.TrackYear;
-                using (MusicPlayerDbContext Context = new MusicPlayerDbContext())
-                {
-                    Track TrackForDB = Context.Tracks.FirstOrDefault(u => u.Name == track.Name);
+                //using (MusicPlayerDbContext Context = new MusicPlayerDbContext())
+                //{
+                    Track TrackForDB = context.Tracks.FirstOrDefault(u => u.Name == track.Name);
                     if (TrackForDB != null && TrackForDB.Name == track.Name)
                     {
                         MessageBox.Show("such track already exists");
@@ -65,8 +73,8 @@ namespace ClientApp
                     else
                     {
                         model.AddTrack(track);
-                        Context.Tracks.Add(track);
-                        Context.SaveChanges();
+                        context.Tracks.Add(track);
+                        context.SaveChanges();
                         MessageBox.Show("Track added!");
                         this.Close();
                         model.TrackGenre = null;
@@ -76,13 +84,14 @@ namespace ClientApp
                         model.TrackAuthor = null;
                     }
 
-
-                }
+                context.SaveChanges();
+                //}
             }
             else
             {
                 MessageBox.Show("Enter information about track!", "Information",
                     MessageBoxButton.OK, MessageBoxImage.Information);
+                
             }
         }
     }
